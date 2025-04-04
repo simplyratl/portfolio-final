78be3971965104abc10091cf68fab8aa21bea741
nikicaraznatovic <nikicaraznatovic123@gmail.com>
2025-04-04 23:04:28 +0200

    feat: enhance tooltip component with customizable className and improved animation

--- src/components/shared/Tooltip.tsx
+++ src/components/shared/Tooltip.tsx
@@ -3,12 +3,14 @@
 import React, { JSX } from 'react';
 import * as TooltipRadix from '@radix-ui/react-tooltip';
 import { motion } from 'motion/react';
+import { cn } from '@/lib/utils';
 
 type Props = {
   text: string;
   position?: 'top' | 'bottom' | 'left' | 'right';
   children: JSX.Element;
   delayDuration?: number;
+  className?: string;
 };
 
 export default function AppTooltip({
@@ -16,22 +18,45 @@ export default function AppTooltip({
   position = 'bottom',
   children,
   delayDuration = 0,
+  className,
 }: Props) {
+  const animationDistance = 8;
+  const isVertical = position === 'top' || position === 'bottom';
+  const isPositive = position === 'top' || position === 'left';
+
+  const animationProps = {
+    initial: {
+      opacity: 0,
+      scale: 0.9,
+      [isVertical ? 'y' : 'x']: isPositive
+        ? animationDistance
+        : -animationDistance,
+    },
+    animate: {
+      opacity: 1,
+      scale: 1,
+      [isVertical ? 'y' : 'x']: 0,
+    },
+  };
+
   return (
     <TooltipRadix.Provider delayDuration={delayDuration}>
       <TooltipRadix.Root>
         <TooltipRadix.Trigger asChild>{children}</TooltipRadix.Trigger>
         <TooltipRadix.Portal>
           <TooltipRadix.Content
-            className='bg-foreground text-background rounded-lg px-2 py-1 text-sm'
+            className={cn(
+              'bg-foreground text-background rounded-lg px-2 py-1 text-sm',
+              className
+            )}
             sideOffset={5}
             side={position}
             asChild
           >
             <motion.div
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 0.1 }}
+              initial={animationProps.initial}
+              animate={animationProps.animate}
+              transition={{ duration: 0.2, ease: 'easeInOut' }}
             >
               {text}
             </motion.div>
